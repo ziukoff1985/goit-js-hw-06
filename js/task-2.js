aa0a76229529423f7e06d1779ad66485c044bb92
Borys Ziukov <ziukoff1985@gmail.com>
2024-09-25 16:22:37 +0300

    added js files and solving of tasks 1, 2, 3

--- js/task-2.js
+++ js/task-2.js
@@ -1,10 +1,46 @@
 'use strict';
 
-function getShippingMessage(country, price, deliveryFee) {
-  const totalPrice = price + deliveryFee;
-  return `Shipping to ${country} will cost ${totalPrice} credits`;
+class Storage {
+  #items;
+
+  constructor(item) {
+    this.#items = item;
+  }
+
+  getItems() {
+    return this.#items;
+  }
+
+  addItem(newItem) {
+    this.#items.push(newItem);
+  }
+
+  removeItem(itemToRemove) {
+    this.#items = this.#items.filter(element => element !== itemToRemove);
+  }
+
+  // АЛЬТЕРНАТИВНИЙ ВАРІАНТ через метод splice()
+
+  // removeItem(itemToRemove) {
+  //   const index = this.#items.indexOf(itemToRemove);
+  //   if (index !== -1) {
+  //     this.#items.splice(index, 1);
+  //   }
+  // }
 }
 
-console.log(getShippingMessage('Australia', 120, 50)); // "Shipping to Australia will cost 170 credits"
-console.log(getShippingMessage('Germany', 80, 20)); // "Shipping to Germany will cost 100 credits"
-console.log(getShippingMessage('Sweden', 100, 20)); // "Shipping to Sweden will cost 120 credits"
+const storage = new Storage(['Nanitoids', 'Prolonger', 'Antigravitator']);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+
+storage.addItem('Droid');
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+
+storage.removeItem('Prolonger');
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+storage.removeItem('Scaner');
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+console.log(
+  '======================================================================'
+);
--- js/task-3.js
+++ js/task-3.js
@@ -1,13 +1,34 @@
 'use strict';
 
-function getElementWidth(content, padding, border) {
-  const contentNumber = Number.parseFloat(content);
-  const paddingNumber = Number.parseFloat(padding);
-  const borderNumber = Number.parseFloat(border);
-  const width = contentNumber + paddingNumber * 2 + borderNumber * 2;
-  return width;
+class StringBuilder {
+  #value;
+
+  constructor(initialValue) {
+    this.#value = initialValue;
+  }
+
+  getValue() {
+    return this.#value;
+  }
+
+  padEnd(str) {
+    this.#value = this.#value.concat(str);
+  }
+
+  padStart(str) {
+    this.#value = str.concat(this.#value);
+  }
+
+  padBoth(str) {
+    this.#value = str.concat(this.#value).concat(str);
+  }
 }
 
-console.log(getElementWidth('50px', '8px', '4px')); // 74
-console.log(getElementWidth('60px', '12px', '8.5px')); // 101
-console.log(getElementWidth('200px', '0px', '0px')); // 200
+const builder = new StringBuilder('.');
+console.log(builder.getValue()); // "."
+builder.padStart('^');
+console.log(builder.getValue()); // "^."
+builder.padEnd('^');
+console.log(builder.getValue()); // "^.^"
+builder.padBoth('=');
+console.log(builder.getValue()); // "=^.^="
